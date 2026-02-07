da110ed983cb77fc62fd6243cb81bc9391f5486f
javadamiri20 <javadamiri2020@gmail.com>
2026-02-07 11:36:07 -0800

    Add files via upload

---
+++ Zoj va Fard.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a Number :"))
+
+if num != 0 and num % 2 == 0:
+    print("Adad Zog Ast.")
+elif num != 0 and num % 2 != 0:
+    print("Adad Manfi Ast.")
+elif num == 0:
+    print("Adad 0 Ast.")
