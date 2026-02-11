0d1fadaff6d27a86310507ce340f4bea6c69b528
javadamiri20 <javadamiri2020@gmail.com>
2026-02-11 09:31:43 -0800

    Delete Zoj va Fard.py

--- Zoj va Fard.py
+++
@@ -1,8 +0,0 @@
-num = int(input("Enter a Number :"))
-
-if num != 0 and num % 2 == 0:
-    print("Adad Zog Ast.")
-elif num != 0 and num % 2 != 0:
-    print("Adad Manfi Ast.")
-elif num == 0:
-    print("Adad 0 Ast.")
